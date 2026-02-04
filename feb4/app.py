7fd87c0fc25b813356245698385d9e0d53c45999
Chad Redmond <credmond@mercyhurst.edu>
2026-02-04 14:47:32 +0000

    feb4

---
+++ feb4/app.py
@@ -0,0 +1,35 @@
+import torch
+
+X = torch.tensor([
+    [3.0]
+])
+
+Y = torch.tensor([
+    [10.0]
+])
+
+w = torch.tensor([
+    [6.0]
+],requires_grad = True)
+
+b = torch.tensor([
+    [1.0]
+],requires_grad = True)
+
+lr = .2
+
+Yhat = X@w+b
+r = Yhat - Y
+SSE = r.T@r
+loss = SSE/1
+
+loss.backward()
+
+with torch.no_grad():
+    w -=  lr*w.grad
+    b -=  lr*b.grad
+
+w.grad.zero_()
+b.grad.zero_()
+
+print(w,b)
